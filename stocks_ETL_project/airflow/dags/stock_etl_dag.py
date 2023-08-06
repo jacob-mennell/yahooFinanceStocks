22fee77ca8c2585e92ae62272b961ff75a8864b8
jmennell <jacob.mennell@britinsurance.com>
2023-08-06 13:28:50 +0100

    folder structure refinement and docker DAG set-up

--- stocks_ETL_project/airflow/dags/stock_etl_dag.py
+++ stocks_ETL_project/airflow/dags/stock_etl_dag.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-
 # set environment variables
 os.environ['SQL_USERNAME'] = ''
 os.environ["SQL_PASSWORD"] = ""
@@ -23,42 +22,47 @@ package_dir = os.path.dirname(parent_dir)
 # Add the package directory to the Python path
 sys.path.append(package_dir)
 
-from dataExtract import *
+from etlClass import ETLClass
 
-# DAG arguments
+def run_etl(stock_list, period, interval):
+    x = ETLClass(stock_list)
+    x.combined_tables()
+    x.exchange_rate_table(period=period, interval=interval)
+
+# DAG arguments with default parameters
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2023, 8, 1, 22, 0),  # Start at 10 PM
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
+    'params': {
+        'stock_list': ['IAG.L', '0293.HK', 'AF.PA'],
+        'period': '1y',
+        'interval': '1d',
+    }
 }
 
 # Create the DAG
 dag = DAG(
     'stock_etl_dag',
     default_args=default_args,
-    start_date = datetime(2023,8,1),
+    start_date=datetime(2023, 8, 1),
     description='My DAG for executing functions once a day at 10 PM',
-    schedule='0 22 * * *',  # Cron expression to run at 10 PM every day
+    schedule_interval='0 22 * * *',  # Cron expression to run at 10 PM every day
 )
 
-# Define tasks for each function execution
-
-# Task for sending individual tables to SQL
-task_combined_stock = PythonOperator(
-    task_id='combined_stock_task',
-    python_callable=combined_stock_sql_send,
-    op_args=[['AAPL', 'GOOG']],  # Stock list
-)
-
-# Task for sending exchange rate table to SQL
-task_exchange_rate = PythonOperator(
-    task_id='exchange_rate_task',
-    python_callable=exchange_rate_table,
-    op_args=[['AAPL', 'GOOG'], '1y', '1d'],  # Pass stock_list, period, and interval
+# Define a task to run the ETL function
+task_run_etl = PythonOperator(
+    task_id='run_etl_task',
+    python_callable=run_etl,
+    op_args=[
+        '{{ params.stock_list }}',
+        '{{ params.period }}',
+        '{{ params.interval }}'
+    ],
     dag=dag,
 )
 
-# Set task dependencies to define the execution order
-task_combined_stock >> task_exchange_rate
+# Set the task dependencies to define the execution order
+task_run_etl
